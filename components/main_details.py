a3c12f89280ac8b05d983ef5a0e24356cdc1acdf
Chanchal Hazra <chanchalhazra@Mac.attlocal.net>
2025-05-22 12:07:28 -0700

    changes on definitions

--- components/main_details.py
+++ components/main_details.py
@@ -6,23 +6,24 @@ import pandas as pd
 def component_yrly_balances(df1, df2, df3, df4):
     with st.container():
         #st.markdown("#### Portfolio Detailed Table")
-        tab1, tab2, tab3, tab4 = st.tabs(["Significantly Below Average", "Below Average", "Average", "Best Case"])
+        tab1, tab2, tab3, tab4 = st.tabs(["Significantly Below Average Market", "Below Average Market Return",
+                                          "Average Market Return", "Best Case Market Return"])
         with tab1:
-            st.caption("90% of simulated market returns are better than these returns")
+            st.caption("In 90% of the simulations, results as good or better than the results shown.")
             st.dataframe(df1,use_container_width=True)
             st.bar_chart(df1[["ending balance","expense"]],use_container_width=True)
 
         with tab2:
-            st.caption("75% of simulated market returns are better than these returns")
+            st.caption("In 75% of the simulations, results as good or better than the results shown.")
             st.dataframe(df2, use_container_width=True)
             st.bar_chart(df2[["ending balance","expense"]], use_container_width=True)
 
         with tab3:
-            st.caption("50% of simulated market returns are better than this value")
+            st.caption("In 50% of the simulations, results as good or better than the results shown.")
             st.dataframe(df3, use_container_width=True)
             st.bar_chart(df3[["ending balance","expense"]], use_container_width=True)
 
         with tab4:
-            st.caption("75% of simulated market returns are better than this value")
+            st.caption("In 25% of the simulations, results as good or better than the results shown.")
             st.dataframe(df4, use_container_width=True)
             st.bar_chart(df4[["ending balance","expense"]], use_container_width=True)
